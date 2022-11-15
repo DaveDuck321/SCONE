76c1ffc9cade53a5945d57ab840a11f6e0094860
Tom Grant <thomas.grant.mail@gmail.com>
2022-11-15 23:07:11 +0000

    Add debugging utilities and (passing) testcase

--- Tallies/TallyClerks/Tests/surfaceCurrentClerk_test.f90
+++ Tallies/TallyClerks/Tests/surfaceCurrentClerk_test.f90
@@ -1,14 +1,15 @@
 module surfaceCurrentClerk_test
 
     use numPrecision
-    use tallyResult_class,               only : tallyResult
-    use surfaceCurrentClerk_class, only : surfaceCurrentClerk, SJResult
+    use dictionary_class,                only : dictionary
+    use multiMap_class,                  only : multiMap
+    use outputFile_class,                only : outputFile
     use particle_class,                  only : particle, particleState, P_NEUTRON
     use particleDungeon_class,           only : particleDungeon
-    use dictionary_class,                only : dictionary
     use scoreMemory_class,               only : scoreMemory
+    use surfaceCurrentClerk_class,       only : surfaceCurrentClerk, SJResult
+    use tallyResult_class,               only : tallyResult
     use testNeutronDatabase_class,       only : testNeutronDatabase
-    use outputFile_class,                only : outputFile
     use pFUnit_mod
 
     implicit none
@@ -17,25 +18,50 @@ module surfaceCurrentClerk_test
     type, extends(TestCase) :: test_surfaceCurrentClerk
       private
       type(surfaceCurrentClerk) :: clerk
-      integer(shortInt) :: N
+      type(multiMap)            :: map
+      integer(shortInt)         :: N
     contains
       procedure :: setUp
       procedure :: tearDown
+
+      !! (Private) test helper function
+      procedure :: printSurfaceCurrents
     end type test_surfaceCurrentClerk
 
   contains
 
     !!
-    !! Test helper: turns a coordinate set into a multimap index
+    !! Test helper: visualize the surface current
     !!
-    integer(shortInt) function getMultiMapIndex(coord, N)
-    implicit none
-    integer(shortInt), dimension(3), intent(in) :: coord
-    integer(shortInt), intent(in) :: N
+    subroutine printSurfaceCurrents(this, currentMatrix, inState)
+      class(test_surfaceCurrentClerk), intent(inout) :: this
+      class(SJResult), intent(in)                    :: currentMatrix
+      type(particleState), intent(in)                :: inState
+      type(particleState)                            :: state
+      integer(shortInt) :: range
+      integer(shortInt) :: idx, x, y, z
+      real(defReal)     :: scale
 
-    getMultiMapIndex = coord(1) + (coord(2) * N) + (coord(3) * (N**2))
+      state = inState
+      scale = 1.0
+      range = this % n / 2
 
-    end function getMultiMapIndex
+      print *, ""
+      print *, "Surface current matrix"
+      do x = -range, range
+        do y = -range, range
+          do z = -range, range
+            state % r = [0.5 + x * scale, 0.5 + y * scale, 0.5 + z * scale]
+            idx = this % map % map(state)
+            if (idx /= 0) then
+              print *, "r: ", state % r, "mapping: ", idx, "  Current: ", currentMatrix % JM (1, idx, 1)
+            end if
+          end do
+        end do
+      end do
+
+    end subroutine printSurfaceCurrents
+
 
     !!
     !! Sets up test_surfaceCurrentClerk object we can use in a number of tests
@@ -43,7 +69,7 @@ module surfaceCurrentClerk_test
     subroutine setUp(this)
       class(test_surfaceCurrentClerk), intent(inout) :: this
       type(dictionary)                                     :: dict
-      type(dictionary)                                     :: multiMap, mapx, mapy, mapz
+      type(dictionary)                                     :: multiMapDict, mapx, mapy, mapz
       character(nameLen)                                   :: name
 
       call mapx % init(6)
@@ -70,28 +96,29 @@ module surfaceCurrentClerk_test
       call mapz % store('max', 1.0_defReal)
       call mapz % store('N', 2)
 
-      call multiMap % init(5)
-      call multiMap % store('type','multiMap')
-      call multiMap % store('maps', ['mapx', 'mapy', 'mapz'])
-      call multiMap % store('mapx', mapx)
-      call multiMap % store('mapy', mapy)
-      call multiMap % store('mapz', mapz)
+      call multiMapDict % init(5)
+      call multiMapDict % store('type','multiMap')
+      call multiMapDict % store('maps', ['mapx', 'mapy', 'mapz'])
+      call multiMapDict % store('mapx', mapx)
+      call multiMapDict % store('mapy', mapy)
+      call multiMapDict % store('mapz', mapz)
 
 
       ! Build intput dictionary
       call dict % init(2)
       call dict % store('type','surfaceCurrentClerk')
-      call dict % store('spacing', 1.0_defReal)
-      call dict % store('spaceMap', multiMap)
+      call dict % store('spacing', [1.0_defReal, 1.0_defReal, 1.0_defReal])
+      call dict % store('spaceMap', multiMapDict)
 
       name = 'testClerk'
       call this % clerk % init(dict, name)
+      call this % map % init(multiMapDict)
       this % N = 2
 
       call mapx % kill()
       call mapy % kill()
       call mapz % kill()
-      call multiMap % kill()
+      call multiMapDict % kill()
       call dict % kill()
     end subroutine setUp
 
@@ -118,12 +145,13 @@ module surfaceCurrentClerk_test
       class(test_surfaceCurrentClerk), intent(inout) :: this
       type(scoreMemory)                                    :: mem
       type(particle)                                       :: p
-      type(particleState)                                  :: phase
+      type(particleState)                                  :: state
       type(particleDungeon)                                :: pop
       type(testNeutronDatabase)                            :: xsData
       real(defReal)                                        :: val
+      integer(shortInt)                                    :: i, idx
       class(tallyResult), allocatable                      :: res
-      integer(shortInt) :: i, idx
+      real(defReal), dimension(3), parameter :: DEFAULT_VELOCITY = [0.0, 0.0, 0.0]
       real(defReal), parameter :: TOL = 1.0E-7
 
 
@@ -139,38 +167,41 @@ module surfaceCurrentClerk_test
 
       ! Score some events
       p % type = P_NEUTRON
+
+      ! Crosses boundary at x=0.0 in +ve direction with weight 1.0
       p % w = 1.0
-
-      ! Crosses boundary at 0.0 (with 0 velocity)
       p % preCollision % r = [-0.1_defReal, 0.1_defReal, 0.1_defReal]
-      call p % coords % init([0.1_defReal, 0.1_defReal, 0.1_defReal], [0.0_defReal, 0.0_defReal, 0.0_defReal])
+      call p % coords % init([0.1_defReal, 0.1_defReal, 0.1_defReal], DEFAULT_VELOCITY)
+      call this % clerk % reportInColl(p, xsData, mem)
 
+      ! Crosses boundary at x=0.0 in -ve direction with weight 0.25
+      p % w = 0.25
+      p % preCollision % r = [0.1_defReal, 0.1_defReal, 0.1_defReal]
+      call p % coords % init([-0.1_defReal, 0.1_defReal, 0.1_defReal], DEFAULT_VELOCITY)
       call this % clerk % reportInColl(p, xsData, mem)
 
       ! Close cycle
       call mem % closeCycle(ONE)
 
-      ! Verify results (TODO)
-      ! print *, val
-      ! do i=0, 3*2*2*2
-      !   call mem % getResult(val, 1_longInt + i)
-      !   print *, val
-      ! end do
-      ! @assertEqual(0.22580645161_defReal, val, TOL)
-
-      ! Verify run-time result
+      ! Verify run-time results
       call this % clerk % getResult(res, mem)
 
       select type(res)
         class is (SJResult)
           @assertEqual(res % N, (this % N) ** 3)
 
-          idx = 1 + getMultiMapIndex([1, 1, 1], this % N)
-          ! print *, ""
-          ! do i=1, 2*2*2
-          !   print *, res % JM(1, i, 1)
-          ! end do
-          @assertEqual(1.0_defReal, res % JM(1, idx, 1), TOL)
+          ! Test current corresponding to the x=0 boundary
+          state = p
+          state % r = [-0.5, 0.5, 0.5]
+          idx = this % map % map(state)
+          @assertEqual(0.75_defReal, res % JM(1, idx, 1), TOL)
+
+          ! Confirm no other currents have been recorded
+          do i=1, (this % N ** 3)
+            if (i /= idx) then
+              @assertEqual(0.0_defReal, res % JM(1, i, 1), TOL)
+            end if
+          end do
         class default
           @assertEqual(0, 1)
       end select
@@ -181,4 +212,4 @@ module surfaceCurrentClerk_test
 
     end subroutine testSimpleUseCase
 
-  end module surfaceCurrentClerk_test
+end module surfaceCurrentClerk_test
