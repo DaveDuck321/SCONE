9f55ebdfcd188961411211f21c4557f164b8e74c
Tom Grant <thomas.grant.mail@gmail.com>
2022-11-16 03:03:37 +0000

    Visualize Shannon Entropy along with fission events

--- Presentation/neutron_evolution.py
+++ Presentation/neutron_evolution.py
@@ -4,13 +4,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 
+from entropyOutput import shannonEntropy
+
+
 def read_generation_positions_from_file(file):
     generations = []
 
     # https://stackoverflow.com/questions/4703390/how-to-extract-a-floating-number-from-a-string
-    floating_point_rx = re.compile('[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?', re.VERBOSE)
+    floating_point_rx = re.compile(
+        "[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?", re.VERBOSE
+    )
 
-    with open(file, 'r') as file:
+    with open(file, "r") as file:
         next_gen = []
         for line in file:
             line = line.strip()
@@ -20,23 +25,45 @@ def read_generation_positions_from_file(file):
                 continue
 
             numbers_on_line = list(map(float, floating_point_rx.findall(line)))
-            next_gen.append(numbers_on_line[0: 2])
+            next_gen.append(numbers_on_line[0:2])
 
+            # Limit animation length
+            if len(generations) >= 200:
+                break
     return np.array(generations)
 
 
-positions = read_generation_positions_from_file('positions_large_high_pop.txt')
+positions = read_generation_positions_from_file("positions_large_high_pop.txt")
 
 fig = plt.figure()
 zeros = np.zeros(len(positions[0, :, 0]))
-scatter = plt.scatter(zeros, zeros, s=0.1)
 
-def animate_plot(frame):
+(line,) = plt.plot([], color="orange", label="Entropy")
+scatter = plt.scatter(zeros, zeros, s=0.1, label="Fission Events")
+plt.legend()
+
+xdata, ydata = [], []
+
+
+def animate_plot(animation_data):
+    data, frame = animation_data
     scatter.set_offsets(positions[frame, :, :])
 
-anim = animation.FuncAnimation(fig, animate_plot, frames=range(len(positions)))
+    if frame == 0:
+        xdata.clear()
+        ydata.clear()
 
-plt.axis('square')
+    # TODO: nicer parameter generation, this is rather adhoc
+    xdata.append(frame * 4 - 450)
+    ydata.append(data * 70 - 550)
+    line.set_data(xdata, ydata)
+
+
+anim = animation.FuncAnimation(
+    fig, animate_plot, frames=zip(shannonEntropy, range(len(positions))), save_count=200
+)
+
+plt.axis("square")
 plt.xlim([-500, 500])
 plt.ylim([-500, 500])
 anim.save("evolution.mp4")
